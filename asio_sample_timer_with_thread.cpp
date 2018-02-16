cb0f8099676a74619df26ca4773d02e2cd0da350
Andrew Nwawudu <gide@btinternet.com>
2018-02-16 14:37:14 +0000

    Added timer with thread example

---
+++ asio_sample_timer_with_thread.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+#include <boost/chrono/time_point.hpp>
+#include <boost/chrono/io/time_point_io.hpp>
+#include <boost/chrono/chrono.hpp>
+#include <boost/system/error_code.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+
+void timer_expired(std::string id) {
+    std::cout << boost::chrono::system_clock::now() << " " << id << " enter.\n";
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    std::cout << boost::chrono::system_clock::now() << " " << id << " leave.\n";
+}
+
+int main() {
+    // Slushie shack
+    boost::asio::io_context context;
+
+    boost::asio::deadline_timer timer1(context, boost::posix_time::seconds(5));
+    boost::asio::deadline_timer timer2(context, boost::posix_time::seconds(5));
+
+    // Non blocking call, Jonny. When timeout finishes calls completion handler to print ' ...timer expired'
+    // Timer begins, waits 5 second, places handler on completion queue.
+    timer1.async_wait( [](auto ... vn) { timer_expired("timer1"); } );
+    timer2.async_wait( [](auto ... vn) { timer_expired("timer2"); } );
+
+    // Thread that services the completion handler. Butler to slushy shack, blocks wait for all services to complete
+    std::thread butler( [&]() { context.run(); } );
+
+    butler.join();
+
+    std::cout << boost::chrono::system_clock::now() << " : done\n";
+}
