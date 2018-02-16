cb14e0046f27255969f3310a5e890393bd08357e
Andrew Nwawudu <gide@btinternet.com>
2018-02-16 14:06:06 +0000

    Added info.cpp to build, updated to use Boost 1.66

--- info.cpp
+++ info.cpp
@@ -1,6 +1,18 @@
-
 #include <vector>
+#include <deque>
+#include <string>
+#include <exception>
+#include <iostream>
 #include <boost/shared_ptr.hpp>
+#include <boost/system/error_code.hpp>
+#include <boost/asio.hpp>
+#include <boost/asio/strand.hpp>
+#include <boost/asio/bind_executor.hpp>
+
+// See for changes to interface in 1.66
+// http://www.boost.org/doc/libs/1_66_0/doc/html/boost_asio/net_ts.html
+//
+//
 
 // *** SERVER ***
 template <typename ConnectionHandler>
@@ -8,30 +20,29 @@ class asio_generic_server {
   using shared_handler_t = std::shared_ptr<ConnectionHandler>;
   public:
     asio_generic_server(int thread_count=1) : 
-          thread_count_(thread_count), acceptor_(io_service_) {
+          thread_count_(thread_count), acceptor_(io_context_) {
     }
     
     void start_server(uint16_t port);
   private:
     void handle_new_connection( 
       shared_handler_t handler, 
-      system::error_code const & error ) {
-    }
+      boost::system::error_code const & error );
     
     int thread_count_;
     std::vector<std::thread> thread_pool_;
-    boost:asio::io_service io_service_;
-    boost::asio::ip::tcp::acceptor acceptor_; # Listening
+    boost::asio::io_context io_context_;
+    boost::asio::ip::tcp::acceptor acceptor_; // Listening
 };
 
-
-void asio_generic_server::start_server(uint16_t port) {
-  auto handler = std::make_shared<ConnectionHandler>(io_service_);
+template<typename ConnectionHandler>
+void asio_generic_server<ConnectionHandler>::start_server(uint16_t port) {
+  auto handler = std::make_shared<ConnectionHandler>(io_context_);
 
   // set up the acceptor to listen on the tcp port
   boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::tcp::v4(), port);
   acceptor_.open(endpoint.protocol());
-  acceptor_.set_option(tcp::acceptor::reuse_address(true)); //???
+  acceptor_.set_option(boost::asio::ip::tcp::acceptor::reuse_address(true));
   acceptor_.bind(endpoint);
   acceptor_.listen();
   
@@ -41,21 +52,20 @@ void asio_generic_server::start_server(uint16_t port) {
     }
   );
 
-
   // start pool of threads to process the asio events
   for(int i=0; i < thread_count_; ++i) {
-    thread_pool_.emplace_back( [=]{io_service_.run();} );
+    thread_pool_.emplace_back( [=]{io_context_.run();} );
   }
 }
 
-
-void asio_generic_server::handle_new_connection( 
-    shared_handler_t handler, system::error_code const & error ) {
+template<typename ConnectionHandler>
+void asio_generic_server<ConnectionHandler>::handle_new_connection( 
+    shared_handler_t handler, boost::system::error_code const & error ) {
   if(error){ return; }
   
   handler->start();
 
-  auto new_handler = std::make_shared<ConnectionHandler>(io_service_);
+  auto new_handler = std::make_shared<ConnectionHandler>(io_context_);
   
   acceptor_.async_accept(
       new_handler->socket(), [=](auto ec) {
@@ -68,8 +78,8 @@ void asio_generic_server::handle_new_connection(
 // this allow us to control our own lifetime.
 class chat_handler : public std::enable_shared_from_this<chat_handler> {
   public:
-    chat_handler(asio::io_service& service) : 
-           service_(service), socket_(service), write_strand_(service) {
+    chat_handler(boost::asio::io_context& context) : 
+           context_(context), socket_(context), write_strand_(context) {
     }
     
     boost::asio::ip::tcp::socket& socket() {
@@ -80,19 +90,26 @@ class chat_handler : public std::enable_shared_from_this<chat_handler> {
       read_packet(); // Start our operations
     }
     
-    void send(std::string msg){
-      service_.post( write_strand_.wrap( [me=shared_from_this()]() {
-        me->queue_message(msg);
-      } ));
+    void send(std::string msg) {
+//       context_.post( write_strand_.bind_executor(  // aka wrap
+//             [me=shared_from_this(),msg]() {
+//                 me->queue_message(msg);
+//             } ));
+// https://github.com/boostorg/asio/issues/79
+
+      boost::asio::post(context_, 
+          boost::asio::bind_executor(write_strand_, [me=shared_from_this(),msg]() {
+                                                       me->queue_message(msg);
+                                                    } 
+                                    )
+                       );
     }
-    
   private:
-    
     void read_packet();
-    void read_packet_done( system::error_code const& error, std::size_t bytes_transferred ) ;
+    void read_packet_done( boost::system::error_code const& error, std::size_t bytes_transferred ) ;
     
     void start_packet_send();
-    void packet_send_done(system::error_code const& error);
+    void packet_send_done(boost::system::error_code const& error);
     
     void queue_message(std::string message) {
       bool write_in_progress = !send_packet_queue.empty();
@@ -103,21 +120,21 @@ class chat_handler : public std::enable_shared_from_this<chat_handler> {
       }
     }
     
-    boost::asio::io_service& service_;
+    boost::asio::io_context& context_;
     boost::asio::ip::tcp::socket socket_;
-    boost::asio::io_service::strand write_strand_; // Prevents multiple writes to port
+    boost::asio::io_context::strand write_strand_; // Prevents multiple writes to port
     boost::asio::streambuf in_packet_;
     std::deque<std::string> send_packet_queue;
 };
 
 
 void chat_handler::read_packet() {
-  asio::async_read_until( 
+  boost::asio::async_read_until( 
       socket_,    // From
       in_packet_, // Destination
-      ’\0’,       // Terminator
+      '\0',       // Terminator
       // Completion handler, me is a shared pointer
-      [me=shared_from_this()]( system::error_code const& ec, std::size_t bytes_xfer)
+      [me=shared_from_this()]( boost::system::error_code const& ec, std::size_t bytes_xfer)
       {
         me->read_packet_done(ec, bytes_xfer);
       } 
@@ -125,7 +142,7 @@ void chat_handler::read_packet() {
 }
 
 
-void chat_handler::read_packet_done( system::error_code const& error, std::size_t bytes_transferred ) {
+void chat_handler::read_packet_done( boost::system::error_code const& error, std::size_t bytes_transferred ) {
   if(error){ return; }
 
   std::istream stream(&in_packet_);
@@ -140,18 +157,27 @@ void chat_handler::read_packet_done( system::error_code const& error, std::size_
 
 void chat_handler::start_packet_send() {
   send_packet_queue.front() += "\0";
-  async_write( socket_, 
-      asio::buffer(send_packet_queue.front()), 
-           write_strand_.wrap( 
-             [me=shared_from_this()]( system::error_code const& ec, std::size_t) {
+//   boost::asio::async_write( socket_, 
+//       boost::asio::buffer(send_packet_queue.front()), 
+//            write_strand_.bind_executor( // aka wrap
+//              [me=shared_from_this()]( boost::system::error_code const& ec, std::size_t) {
+//                 me->packet_send_done(ec);
+//               }
+//            )
+//          );
+  
+  boost::asio::async_write( socket_, 
+      boost::asio::buffer(send_packet_queue.front()), 
+        boost::asio::bind_executor(write_strand_, 
+              [me=shared_from_this()]( boost::system::error_code const& ec, std::size_t) {
                 me->packet_send_done(ec);
               }
-           )
-         );
+        )
+    ) ;
 }
 
 
-void chat_handler::packet_send_done(system::error_code const& error) {
+void chat_handler::packet_send_done(boost::system::error_code const& error) {
   if(!error) {
     send_packet_queue.pop_front();
     if(!send_packet_queue.empty()) { 
@@ -162,8 +188,11 @@ void chat_handler::packet_send_done(system::error_code const& error) {
 
 
 int main() {
-  // Instinate new chat_handler each time client connects
-  asio_generic_server<chat_handler> server;
-  server.start_server(8888);
+  try {
+      // Instinate new chat_handler each time client connects
+      asio_generic_server<chat_handler> server;
+      server.start_server(5050);
+  } catch(std::exception& e) {
+      std::cout << e.what() << std::endl ;
+  }
 }
-
