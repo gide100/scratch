019f9bf4aaee08a59fe498377881454e73119e80
Andrew Nwawudu <gide@btinternet.com>
2018-02-23 20:59:17 +0000

    Added date conversion functions, default locale, and security_id type

--- types.hpp
+++ types.hpp
@@ -5,6 +5,8 @@
 #include <string>
 #include <cstdint>
 #include <ctime>
+#include <chrono>
+#include "date.h"
 
 namespace an {
 // Orders
@@ -18,12 +20,28 @@ typedef std::string symbol_t;
 const location_t ME = "ME"; // Matching Engine
 
 // Security data
+typedef std::uint32_t security_id_t;
 typedef std::time_t date_t;
 typedef std::uint32_t ladder_id_t;
+
+const std::string MY_LOCALE = "en_GB.UTF-8";
 
 // Constants
 const price_t MAX_SHARE_PRICE = 1000000.0;
 const shares_t MAX_OUTSTANDING_SHARES = 10000000000;
+
+
+inline std::time_t stringToTimeT(const std::string& dateStr, const char* const dateFormat) {
+    std::stringstream str(dateStr);
+    str.imbue( std::locale(MY_LOCALE) );
+    std::chrono::time_point< std::chrono::system_clock, std::chrono::seconds > result;
+    date::from_stream( str, dateFormat, result );
+    return result.time_since_epoch().count() ;
+}
+
+inline std::time_t dateToTimeT(const std::string& dateStr) {
+    return stringToTimeT(dateStr,"%Y-%m-%d"); // 1999-12-31
+}
 
 } // an - namespace
 
