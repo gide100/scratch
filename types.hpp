954569a31f44bea383294459259628368daa8f3f
Andrew Nwawudu <gide@btinternet.com>
2018-02-24 00:47:12 +0000

    Added date output both formatted and standard

--- types.hpp
+++ types.hpp
@@ -23,6 +23,7 @@ const location_t ME = "ME"; // Matching Engine
 typedef std::uint32_t security_id_t;
 typedef std::time_t date_t;
 typedef std::uint32_t ladder_id_t;
+const   std::time_t MY_MAX_DATE = 0x7FFFFFFF;
 
 const std::string MY_LOCALE = "en_GB.UTF-8";
 
@@ -41,6 +42,19 @@ inline std::time_t stringToTimeT(const std::string& dateStr, const char* const d
 
 inline std::time_t dateToTimeT(const std::string& dateStr) {
     return stringToTimeT(dateStr,"%Y-%m-%d"); // 1999-12-31
+}
+
+inline std::string timeTtoString(const std::time_t& t,  const char* const dateFormat) {
+   auto dt = date::floor<std::chrono::milliseconds>(std::chrono::system_clock::from_time_t(t));
+   std::stringstream ss; ss << date::format(dateFormat,dt);
+   return ss.str();
+}
+
+inline std::string dateToString(const std::time_t& t) {
+   auto d = date::floor<date::days>(std::chrono::system_clock::from_time_t(t));
+   std::stringstream ss; ss << date::format("%F",d);
+   return ss.str();
+   // return timeTtoString(t,"%F");
 }
 
 } // an - namespace
