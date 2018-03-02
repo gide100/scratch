a1d0d6ba86d02b5d88403d9340709deade2e84ff
Andrew Nwawudu <gide@btinternet.com>
2018-03-02 19:48:58 +0000

    Added the concept of an order book, with buy/sell sides. As well as symbol and order lookups

--- matching_engine.cpp
+++ matching_engine.cpp
@@ -7,7 +7,7 @@
 an::MatchingEngine::MatchingEngine(const an::location_t& exchange, SecurityDatabase& secdb)
              : seq_(1), startTime_(std::chrono::steady_clock::now()), exchange_(exchange), secdb_(secdb) { 
     for (const auto& sec : secdb.securities() ) {
-        side_.push_back(Side{sec.symbol,Book(),Book()});
+        book_.push_back(Book{sec.symbol,active_order_t(),Side(),Side()});
     }
 }
 
@@ -15,18 +15,18 @@ an::MatchingEngine::~MatchingEngine() { }
 
 std::string an::MatchingEngine::to_string() const {
     std::ostringstream os;
-    os << "id     Side Time         Shares Price    Shares Time         Side" << std::endl 
-       << "------+----+------------+------+--------+------+------------+----" << std::endl ;
-    for (const auto& rec: side_[0].sell) {
-       os << boost::format("%1$6d ") % rec.id
-          << "                         " 
+    os << "id     Seq  V Side Time               Shares Price    Shares Time               Side" << std::endl 
+       << "------+----+-+----+------------------+------+--------+------+------------------+----" << std::endl ;
+    for (const auto& rec: book_[0].sell) {
+       os << boost::format("%1$6d %2$4d %3$1c ") % rec.id % rec.seq % (rec.visible ? '*' : ' ')
+          << boost::format("%1$30c ") % ' '
           << boost::format("%1$8.3f ") % rec.price
-          << boost::format("%1$6d %2$12s %3$4s") % rec.shares % sinceToHHMMSS(rec.since) % an::to_string(rec.direction)
+          << boost::format("%1$6d %2$18s %3$4s") % rec.shares % sinceToString(rec.time) % an::to_string(rec.direction)
           << std::endl;
     }
-    for (const auto& rec: side_[0].buy) {
-       os << boost::format("%1$6d ") % rec.id
-          << boost::format("%1$4s %2$12s %3$6d ") % an::to_string(rec.direction) % sinceToHHMMSS(rec.since) % rec.shares 
+    for (const auto& rec: book_[0].buy) {
+       os << boost::format("%1$6d %2$4d %3$1c ") % rec.id % rec.seq % (rec.visible ? '*' : ' ')
+          << boost::format("%1$4s %2$18s %3$6d ") % an::to_string(rec.direction) % sinceToString(rec.time) % rec.shares 
           << boost::format("%1$8.3f") % rec.price
           << std::endl;
     }
@@ -36,34 +36,91 @@ std::string an::MatchingEngine::to_string() const {
 
 
 void an::MatchingEngine::applyOrder(Execution* o) {
-    BookRecord rec;
-    o->pack(rec); // TODO TImestamp and sequence
+    SideRecord rec;
+    o->pack(rec); 
+    rec.time = std::chrono::steady_clock::now();
+    rec.seq = seq_++;
+    rec.visible = true;
     const symbol_t& symbol = o->symbol();
     auto symbol_idx = secdb_.find(symbol);
     std::cout << "SYMBOL=" << symbol << " idx=" << symbol_idx << std::endl;
     assert(symbol_idx != std::numeric_limits<std::size_t>::max()); 
-    assert(side_[symbol_idx].symbol == symbol); // Using the right book
+    assert(book_[symbol_idx].symbol == symbol); // Using the right book
     if (isMarketable(rec)) {
          std::cout << "FOUND Marketable: " << o->to_string(); // TODO
     } else {
          // Add to queue
          if (rec.direction == an::BUY) {
-              side_[symbol_idx].buy.push_back(rec);
+              book_[symbol_idx].buy.push_back(rec);
          } else if (rec.direction == an::SELL) {
-              side_[symbol_idx].sell.push_back(rec);
+              book_[symbol_idx].sell.push_back(rec);
          } else {
              assert(false); // Unknown direction    
          }
+         book_[symbol_idx].active_order.emplace(rec.id, rec.direction);
     }
 }
 
 void an::MatchingEngine::applyOrder(CancelOrder* o) {
+    const symbol_t& symbol = o->symbol();
+    order_id_t id = o->orderId();
+    SideRecord* recPtr = lookupSymbol(symbol, id);
+    recPtr->visible = false; // TODO - add more cancel stuff, cancel thread?
 }
 
 void an::MatchingEngine::applyOrder(AmendOrder* o) {
+    const symbol_t& symbol = o->symbol();
+    order_id_t id = o->orderId();
+    SideRecord* recPtr = lookupSymbol(symbol, id);
+    if (recPtr != nullptr) {
+        const auto& amend = o->amend();
+        switch (amend.field) {
+            case NONE:   break;
+            case PRICE:  if (recPtr->order_type == LIMIT) {
+                             recPtr->price = amend.price; 
+                         } else {
+                             throw OrderError("Invalid amend of market order");
+                         }
+                         break;
+            case SHARES: recPtr->shares = amend.shares; break;
+            default:
+                throw OrderError("Invalid amend");
+        }
+    } else {
+        // TODO- Not found
+    } 
 }
 
-bool an::MatchingEngine::isMarketable(BookRecord& rec) {
+bool an::MatchingEngine::isMarketable(SideRecord& rec) {
     return false; // TODO
 }
 
+struct MatchOrderId { // Functor
+    explicit MatchOrderId(an::order_id_t order_id) : order_id_(order_id) { }
+    bool operator()(const an::SideRecord& rec) const { return rec.id == order_id_; }
+    an::order_id_t order_id_;
+};
+
+an::SideRecord* an::MatchingEngine::lookupSymbol(const symbol_t& symbol, order_id_t id) const {
+    auto symbol_idx = secdb_.find(symbol);
+    const SideRecord* found = nullptr;
+    if (symbol_idx != std::numeric_limits<std::size_t>::max()) { // FOUND
+        std::cout << "LOOKUP=" << symbol << " idx=" << symbol_idx << std::endl;
+        auto search = book_[symbol_idx].active_order.find(id);
+        if (search != book_[symbol_idx].active_order.end()) {
+	    Side::const_iterator it ;
+            if (search->second == an::BUY) {
+                it = std::find_if(book_[symbol_idx].buy.cbegin(),  book_[symbol_idx].buy.cend(),  MatchOrderId(id)); 
+                if (it != book_[symbol_idx].buy.cend()) {
+                    found = &(*it);
+                }
+            } else {
+                it = std::find_if(book_[symbol_idx].sell.cbegin(), book_[symbol_idx].sell.cend(), MatchOrderId(id)); 
+                if (it != book_[symbol_idx].sell.cend()) {
+                    found = &(*it);
+                }
+            }
+        }
+    }
+    return const_cast<SideRecord*>(found);
+}
--- matching_engine.hpp
+++ matching_engine.hpp
@@ -3,16 +3,16 @@
 
 #include "types.hpp"
 #include "order.hpp"
-#include <vector>
 
 namespace an {
 
-typedef std::chrono::microseconds since_t;
+//typedef std::chrono::microseconds since_t;
+typedef std::chrono::steady_clock::time_point since_t;
 
-struct BookRecord {
+struct SideRecord {
     order_id_t  id;
     sequence_t  seq;
-    since_t     since;
+    since_t     time;
     order_t     order_type;
     direction_t direction;
     price_t     price;
@@ -21,8 +21,8 @@ struct BookRecord {
     bool        matched;
 };
 
-static const BookRecord DefaultBookRecord =  
-   { .id=0, .seq=0, .since=since_t(0), .order_type=an::LIMIT, 
+static const SideRecord DefaultSideRecord =  
+   { .id=0, .seq=0, .time=since_t(), .order_type=an::LIMIT, 
      .direction=an::BUY, .price=0.0, .shares=0, .visible=false, .matched=false }; 
     
 class SecurityDatabase ;
@@ -38,38 +38,56 @@ class MatchingEngine {
         void applyOrder(CancelOrder* o);
         void applyOrder(AmendOrder* o);
     private:
-        bool isMarketable(BookRecord& rec);
+        bool isMarketable(SideRecord& rec);
+        SideRecord* lookupSymbol(const symbol_t& symbol, order_id_t id) const;
      
         sequence_t  seq_;
         std::chrono::steady_clock::time_point startTime_;
         an::location_t exchange_;
         an::SecurityDatabase& secdb_;
 
-        inline static std::string sinceToHHMMSS(since_t since) {
+//        // https://stackoverflow.com/questions/17946124/most-simple-way-to-get-string-containing-time-interval
+//        inline static std::string sinceToHHMMSS(since_t since) {
+//            std::ostringstream os;
+//            auto hh = std::chrono::duration_cast<std::chrono::hours>(since);
+//            since -= hh;
+//            auto mm = std::chrono::duration_cast<std::chrono::minutes>(since);
+//            since -= mm;
+//            auto ss = std::chrono::duration_cast<std::chrono::seconds>(since);
+//            since -= ss;
+//            auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(since);
+//            
+//            os << std::setfill('0') << std::setw(2) << hh.count() << ':'
+//               << std::setfill('0') << std::setw(2) << mm.count() << ':' 
+//               << std::setfill('0') << std::setw(2) << ss.count() << '.' 
+//               << std::setfill('0') << std::setw(3) << ms.count() ; 
+//            return os.str();
+//        }
+        
+        inline static std::string sinceToString(since_t time) {
+            using namespace date;
+            using namespace std::chrono;
             std::ostringstream os;
-            auto hh = std::chrono::duration_cast<std::chrono::hours>(since);
-            since -= hh;
-            auto mm = std::chrono::duration_cast<std::chrono::minutes>(since);
-            since -= mm;
-            auto ss = std::chrono::duration_cast<std::chrono::seconds>(since);
-            since -= ss;
-            auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(since);
-            
-            os << std::setfill('0') << std::setw(2) << hh.count() << ':'
-               << std::setfill('0') << std::setw(2) << mm.count() << ':' 
-               << std::setfill('0') << std::setw(2) << ss.count() << '.' 
-               << std::setfill('0') << std::setw(3) << ms.count() ; 
+            // https://stackoverflow.com/questions/18361638/converting-steady-clocktime-point-to-time-t
+            // https://stackoverflow.com/questions/35282308/convert-between-c11-clocks
+            const auto system_now = system_clock::now() ;
+            const auto steady_now = steady_clock::now();
+            system_clock::time_point myTime = time_point_cast<system_clock::duration>(time - steady_now + system_now);
+            os << format("%T",myTime);
+            // os << "HH:MM:SS.SSSS";
             return os.str();
         }
 
-        typedef std::vector<BookRecord> Book;
-        //std::priority_queue<BookRecordOrder, std::deque<BookRecord>,
-        struct Side {
+        typedef std::vector<SideRecord> Side;
+        typedef std::unordered_map<order_id_t, an::direction_t> active_order_t;
+        //std::priority_queue<SideRecordOrder, std::deque<SideRecord>,
+        struct Book {
             symbol_t symbol;
-            Book buy;
-            Book sell;
+            active_order_t active_order;
+            Side buy;
+            Side sell;
         } ;
-        std::vector<Side> side_;
+        std::vector<Book> book_;
 };
 
 
