7c1289fc973d0eb660c1ee81c43173861e0a0ac2
Andrew Nwawudu <gide@btinternet.com>
2018-03-05 19:19:07 +0000

    Minor typo changes

--- example.cpp
+++ example.cpp
@@ -7,11 +7,11 @@ int main()
     std::string line;
     boost::regex pat( "^Subject: (Re: |Aw: )*(.*)" );
 
-    while (std::cin)
-    {
+    while (std::cin) {
         std::getline(std::cin, line);
         boost::smatch matches;
-        if (boost::regex_match(line, matches, pat))
+        if (boost::regex_match(line, matches, pat)) {
             std::cout << matches[2] << std::endl;
+        }
     }
 }
