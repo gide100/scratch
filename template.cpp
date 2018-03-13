e3d8f9ce944a0399b98ba31ce3bb5802b8342feb
Andrew Nwawudu <gide@btinternet.com>
2018-03-13 22:16:11 +0000

    Added another example

--- template.cpp
+++ template.cpp
@@ -5,6 +5,7 @@ struct Currency
   int Dollar;
   int Cents;
   Currency() : Dollar(0), Cents(0) {}
+  Currency( int dollar, int cents=0) : Dollar(dollar), Cents(cents) {}
 
   /*REMOVED : operator double();*/
   operator double()  {
@@ -38,9 +39,70 @@ double GetAverage(T tArray[], int nElements)
 }
 
 
-int main() {
-  Currency currency[10];
-  currency[0].Dollar = 20;
-  auto res = GetAverage<Currency>(currency, 10);
-  std::cout << res << std::endl;
+template<typename T>
+class Item {
+    public:
+        Item();
+        void SetData(T data);
+        void PrintData();
+        Item<T>& operator+=( const Item<T>& other ); 
+        Item<T>& operator/=( const Item<T>& other ); 
+
+        template <typename U>
+        void SetAverageFrom(U values[10], int size);
+    private:
+        T Data;
+};
+
+template<typename T>
+Item<T>::Item() : Data( T() ) {
 }
+ 
+template<typename T>
+void Item<T>::SetData(T data) {
+    Data = data;
+}
+template<typename T>
+void Item<T>::PrintData() {
+    std::cout << Data;
+}
+
+template<typename T>
+Item<T>& Item<T>::operator+=( const Item<T>& other ) { 
+    Data += other.Data;
+    return *this;
+}
+
+template<typename T>
+Item<T>& Item<T>::operator/=( const Item<T>& other ) { 
+    Data /= other.Data;
+    return *this;
+}
+
+template<typename T>
+    template<typename U>
+    void Item<T>::SetAverageFrom(U values[10], int size) {
+    U item;
+    for (int i =0; i < size;++i) {
+        item += values[i];
+    }
+    Data /= (double)size;
+}
+
+int main() {
+  //Currency currency[10];
+  //currency[0].Dollar = 20;
+  //auto res = GetAverage<Currency>(currency, 10);
+  //std::cout << res << std::endl;
+    const int Size = 10;
+    //Item<long> Values[Size]; 
+    Item<Currency> Values[Size]; 
+ 
+    for(int nItem = 0; nItem < Size; ++nItem) {
+        Values[nItem].SetData(nItem*40);
+    }
+          
+    Item<float> FloatItem;
+    FloatItem.SetAverageFrom(Values, Size);
+}
+
