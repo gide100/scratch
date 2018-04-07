cf3bc555c414139f3e4e3884526c2e478afc10bd
Andrew Nwawudu <gide@btinternet.com>
2018-04-07 17:54:01 +0100

    Moved timed interval code from shutdown ctor, MyShutdown creates timer. Using const for timer interval. Change dealine to steady timer and using chrono timings.

--- shutdown.cpp
+++ shutdown.cpp
@@ -1,5 +1,20 @@
 #include "shutdown.hpp"
 
+class MyShutdown: public Shutdown {
+    public:
+        const std::chrono::seconds WAIT_TIME = std::chrono::seconds(1);
+        MyShutdown() : Shutdown(), doWaitTimer_(signalContext_, WAIT_TIME)  {
+            doWaitTimer_.async_wait( DoWait(doWaitTimer_, signal_not_received_, WAIT_TIME) );
+        }
+        virtual ~MyShutdown() { }
+    protected:
+        virtual void myHandleStop(const boost::system::error_code& error, int signal_number) {
+            std::cout << "Executing Safe Shutdown signal=" << signal_number << std::endl;
+        }
+    private:
+        boost::asio::steady_timer doWaitTimer_;
+};
+
 int main() {
     MyShutdown safeShutdown;
     safeShutdown.join();
--- shutdown.hpp
+++ shutdown.hpp
@@ -6,40 +6,44 @@
 #include <boost/asio.hpp>
 
 
+// Every second (wait_time) check if signal has been received
 class DoWait {
     public:
-        DoWait(boost::asio::deadline_timer& timer, std::atomic<int>& count) : timer_(timer), count_(count), wait_time_(1) {}
+        DoWait(boost::asio::steady_timer& timer, std::atomic<int>& count, std::chrono::seconds wait_time) 
+            : timer_(timer), count_(count), wait_time_(wait_time) {}
         void operator()(const boost::system::error_code& ec) {
             std::cerr << "[DoWait] iteration = " << count_ << std::endl;
             if (!ec && count_ > 0) {
                 ++count_;
-                timer_.expires_at(timer_.expires_at() + wait_time_);
-                timer_.async_wait( DoWait(timer_, count_) );
+                timer_.expires_at(timer_.expires_at() + wait_time_); // Reset in wait_time seconds
+                timer_.async_wait( DoWait(timer_, count_, wait_time_) ); // Resubmit to queue
             } else {
                 // DO NOTHING, don't resubmit to queue.
             }
         }
     private:
-        boost::asio::deadline_timer& timer_;
+        boost::asio::steady_timer& timer_;
         std::atomic<int>& count_;
-        boost::posix_time::seconds wait_time_;
+        const std::chrono::seconds wait_time_;
 };
 
 
+// Register signals, reset counter when signal is received
 class Shutdown {
     public:
         Shutdown()
             : signal_not_received_(1),
               signalContext_(),
-              signals_(signalContext_, SIGINT, SIGTERM, SIGQUIT),
-              doWaitTimer_(signalContext_, boost::posix_time::seconds(1)) {
+              signals_(signalContext_) {
             std::cout<<"constructor"<<std::endl;
+
+            // Setup signals
+            signals_.add(SIGINT);
+            signals_.add(SIGTERM);
+            signals_.add(SIGQUIT);
             signals_.add(SIGILL);
             signals_.add(SIGHUP);
-
-            // Setup timer and signals
             signals_.async_wait( [&] (const boost::system::error_code& ec, int s) { handleStop(ec, s); } );
-            doWaitTimer_.async_wait( DoWait(doWaitTimer_, signal_not_received_) );
             signalThread_ = std::thread( [&]() { signalContext_.run(); } );
         }
 
@@ -49,6 +53,8 @@ class Shutdown {
             signalContext_.stop();
             //signalThread_.join();
         }
+        Shutdown(const Shutdown& shut) = delete;
+        Shutdown& operator=(const Shutdown& shut) = delete;
 
         bool isSignalReceived() const {
             return signal_not_received_!=0;
@@ -58,33 +64,20 @@ class Shutdown {
             std::cout << "Join (wait)" << std::endl;
             signalThread_.join();
         }
-    private:
-        std::atomic<int> signal_not_received_; // Zero idicates signal received
-        std::atomic<int> wait_time_;
-        boost::asio::io_context signalContext_;
-        std::thread signalThread_;
-        boost::asio::signal_set signals_;
-
-        boost::asio::deadline_timer doWaitTimer_;
+    protected:
+        // TODO: Override this to call shutdown code
+        virtual void myHandleStop(const boost::system::error_code& error, int signal_number) = 0;
 
         void handleStop(const boost::system::error_code& error, int signal_number) {
             signal_not_received_ = 0;
             myHandleStop(error, signal_number);
         }
 
-        virtual void myHandleStop(const boost::system::error_code& error, int signal_number) = 0;
-};
-
-class MyShutdown: public Shutdown {
-public:
-    MyShutdown() : Shutdown() {}
-    virtual ~MyShutdown() { }
-    MyShutdown(const MyShutdown& shut) = delete;
-    MyShutdown& operator=(const MyShutdown& shut) = delete;
-private:
-    void myHandleStop(const boost::system::error_code& error, int signal_number) {
-        std::cout << "Executing Safe Shutdown signal=" << signal_number << std::endl;
-    }
+        std::atomic<int> signal_not_received_; // Zero indicates signal received
+        boost::asio::io_context signalContext_;
+        std::thread signalThread_;
+        boost::asio::signal_set signals_;
 };
 
 #endif
+
